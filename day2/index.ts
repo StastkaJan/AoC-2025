551f6b4efe1a2f231d887ae01c519380db049841
Jan Stastka <czsantafa@gmail.com>
2025-12-02 14:53:07 +0100

    feat(day2)

---
+++ day2/index.ts
@@ -0,0 +1,67 @@
+import input from './input.txt'
+import { trackTime } from '../util/trackTime'
+import { logResultWithTime } from '../util/log'
+
+// Part 1
+const part1 = async (input: string) => {
+  const data = input.split(',').filter(Boolean)
+
+  let res = 0
+  for await (const i of data) {
+    const [start, end] = i.split('-')
+
+    // remove odd digit counts
+    const numbers = Array.from({ length: +end! - +start! + 1 }, (_, i) => +start! + i).filter(
+      (num) => String(num).length % 2 === 0
+    )
+    for await (const num of numbers) {
+      const numberAsString = String(num)
+      const firstHalf = numberAsString.substring(0, numberAsString.length / 2)
+      if (numberAsString !== firstHalf.repeat(2)) continue
+
+      res += num
+    }
+  }
+
+  return res
+}
+
+// Part 2
+const part2 = async (input: string) => {
+  const data = input.split(',').filter(Boolean)
+
+  let res = 0
+  for await (const i of data) {
+    const [start, end] = i.split('-')
+
+    const numbers = Array.from({ length: +end! - +start! + 1 }, (_, i) => +start! + i)
+    for await (const num of numbers) {
+      const numberAsString = String(num)
+      const numberAsStringLength = numberAsString.length
+      const halfLength = Math.floor(numberAsStringLength / 2)
+
+      let isValid = false
+      for (let j = 1; j <= halfLength; j++) {
+        const substring = numberAsString.substring(0, j)
+        if (numberAsString !== substring.repeat(numberAsStringLength / substring.length)) continue
+        isValid = true
+        break
+      }
+
+      if (!isValid) continue
+
+      res += num
+    }
+  }
+
+  return res
+}
+
+export default async () => {
+  trackTime(async () => part1(input)).then(({ result, time }) =>
+    logResultWithTime('Part 1:', result, time)
+  )
+  trackTime(async () => part2(input)).then(({ result, time }) =>
+    logResultWithTime('Part 2:', result, time)
+  )
+}
